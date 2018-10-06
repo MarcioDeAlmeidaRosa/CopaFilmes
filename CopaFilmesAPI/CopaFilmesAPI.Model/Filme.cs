69ba0f69db3eb2ca274ecaa069983a5c17b06595
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2018-10-06 12:11:40 -0300

    INCLUSÃO INICIAL DO PROJETO

---
+++ CopaFilmesAPI/CopaFilmesAPI.Model/Filme.cs
@@ -0,0 +1,20 @@
+﻿namespace CopaFilmesAPI.Model
+{
+    public class Filme
+    {
+        public string ID { get; set; }
+
+        public string Titulo { get; set; }
+
+        public int Ano { get; set; }
+
+        public double Nota { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Filme outroObj))
+                return -1;
+            return Nota.CompareTo(outroObj.Nota);
+        }
+    }
+}
---
+++ CopaFilmesAPI/CopaFilmesAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace CopaFilmesAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            //setando não retorno de propriedade null
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+            //configura o padrão para a serialização das classes do projeto
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver() { };
+            //configurando ignore data anannotation nas propriedades
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
