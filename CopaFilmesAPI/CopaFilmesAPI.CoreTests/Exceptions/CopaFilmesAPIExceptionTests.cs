45a4e57caf9deed2c56fc73055fd0078af34d894
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2018-10-09 00:51:14 -0300

    INCLUSÃO PROJETO DE TESTE DO CORE

---
+++ CopaFilmesAPI/CopaFilmesAPI.CoreTests/Exceptions/CopaFilmesAPIExceptionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CopaFilmesAPI.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopaFilmesAPI.Core.Exceptions.Tests
+{
+    [TestClass()]
+    public class CopaFilmesAPIExceptionTests
+    {
+        [TestMethod()]
+        public void CopaFilmesAPIExceptionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CopaFilmesAPIExceptionTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ CopaFilmesAPI/CopaFilmesAPI.Tests/FilmeControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using CopaFilmesAPI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopaFilmesAPI.Tests
+{
+    [TestClass()]
+    public class FilmeControllerTests
+    {
+        /// Valida a resposta da consulta de filmes, ela não pode ser nula
+        /// </summary>
+        [TestMethod()]
+        public async Task ValidaRetornoNaoNullo()
+        {
+            FilmeController ctrl = new FilmeController();
+            var resultado = await ctrl.Get();
+            Assert.AreNotEqual(resultado, null);
+        }
+
+        /// <summary>
+        /// Valida quantidade de filmes retornados pela consulta
+        /// </summary>
+        [TestMethod()]
+        public async Task ValidaQuantidadeRegistroRetornados()
+        {
+            FilmeController ctrl = new FilmeController();
+            var resultado = await ctrl.Get();
+            Assert.AreEqual(resultado.ToList().Count, 16);
+        }
+
+        /// <summary>
+        /// Valida nota do primeiro filme da lista
+        /// </summary>
+        [TestMethod()]
+        public async Task ValidaNotaPrimeiroRegistroLista()
+        {
+            FilmeController ctrl = new FilmeController();
+            var resultado = await ctrl.Get();
+            Assert.AreEqual(resultado.ToList()[0].Nota, 8.5, 0.0001);
+        }
+
+        /// <summary>
+        /// Valida nota do ultimo filme da lista
+        /// </summary>
+        [TestMethod()]
+        public async Task ValidaNotaUltimoRegistroLista()
+        {
+            FilmeController ctrl = new FilmeController();
+            var resultado = await ctrl.Get();
+            Assert.AreEqual(resultado.ToList()[resultado.Count() - 1].Nota, 7.8, 0.0001);
+        }
+    }
+}
